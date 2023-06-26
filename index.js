39347ac3f1d74fb8d844d6025e6ac90f966387c5
elbaqa <beqa488@gmail.com>
2023-06-26 18:31:42 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const topnav = document.getElementById('top-nav');
+const menu = document.getElementById("menu");
+const clos = document.getElementById("close");
+var x = window.matchMedia("(min-width: 768px)")
+if(x.matches){
+    
+}
+function navbar(){
+
+        if(topnav.style.visibility == "hidden"){
+            topnav.style.visibility = "visible";
+            menu.style.display = "none";
+            clos.style.display = "block";
+        } else{
+            topnav.style.visibility = "hidden";
+            menu.style.display = "block";
+            clos.style.display = "none";
+        }
+} 
+
+document.addEventListener('scroll', scheader)
+const header = document.getElementById("header");
+
+function scheader(){
+    if(scrollY > 300){
+        header.classList.add("onscroll")
+        header.style.transition ="2s"
+    }else{
+        header.classList.remove("onscroll")
+    }
+}
+
+
